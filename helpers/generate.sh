8d2e9e636ddc8f2e2079d205ad405d4775a78268
Denis Tokarchuk <denis.tokarchuk.a@gmail.com>
2023-10-27 12:29:35 +0300

    add auto convert UTF16 to UTF8

--- helpers/generate.sh
+++ helpers/generate.sh
@@ -1,22 +1,33 @@
 #!/usr/bin/bash
 # comment
-STRING="generate wireshark file from txt"
+SCRIPT_NAME="generate wireshark file from txt"
+#HELPERS_DIR=/mnt/d/Work/Programming/dlms/dlms_log
 HELPERS_DIR=/mnt/d/Work/Programming/dlms/wireshark_dlms/helpers
 #print on a screen
-echo $STRING
+echo $SCRIPT_NAME
+
+cd $HELPERS_DIR
 
 # 1. remove all files *.bin type
 echo $(find $HELPERS_DIR -name "*.bin" -delete)
 
-# 2. change target input file name to in_log.txt
+# 2. copy target input file into in_log.txt cause test.py exspects such file
 cp in_$1.txt in_log.txt
 
-# 3. run python script
+# 3. check if encoding is utf-16le then convert to utf-8
+FILE_INFO=$(file in_log.txt)
+SUB_INFO='UTF-16'
+if [[ "$FILE_INFO" == *"$SUB_INFO"* ]]; then
+	echo "Convert from utf-16 encoding to utf-8"
+	iconv -o in_log.txt -f UTF-16LE -t UTF-8 in_log.txt
+fi
+
+# 4. run python script
 echo $(python3 $HELPERS_DIR/test.py)
 
-# 4. create .pcap wireshark file from *.bin files contained packets data
+# 5. create .pcap wireshark file from *.bin files contained packets data
 echo $(find $HELPERS_DIR -name "*.bin" -exec od -Ax -tx1 -v {} \; | text2pcap -l 147 - $HELPERS_DIR/dlms_$1.pcap)
 
-# 5. remove all files *.bin type and in_log.txt
+# 6. remove all files *.bin type and in_log.txt
 echo $(find $HELPERS_DIR -name "*.bin" -delete)
 rm in_log.txt
